4067e0306c9f3fd8b4f11caf46e1559b72be384c
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2022-10-27 17:23:51 +0200

    ok123456

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { PHASE_PRODUCTION_SERVER } = require("next/constants");
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return {
-      ...defaultConfig,
-    };
-  }
-
-  return defaultConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -12,16 +12,12 @@ import "../public/css/responsive.css";
 import "../public/css/rtl.css";
 
 import Layout from "../components/_App/Layout";
-import { Provider } from "react-redux";
-import { useStore } from "../store";
 
 export default function App({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState);
-
   return (
-    <Provider store={store}>
+    <>
       <Layout />
       <Component {...pageProps} />
-    </Provider>
+    </>
   );
 }
