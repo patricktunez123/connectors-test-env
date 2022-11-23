585f8df442dcb9c39ff171dbf961e8729224593b
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2022-11-23 14:17:41 +0200

    ref123456

--- components/Connectors/FunFacts.js
+++ components/Connectors/FunFacts.js
@@ -6,22 +6,34 @@ const FunFacts = () => {
     <div className="funfacts-area bg-image ptb-80">
       <div className="container">
         <div className="section-title">
-          <h2>OUR COMMITMENT</h2>
+          <h2>Our commitment</h2>
           <div className="bar"></div>
           <p>
-            To help African solutions emerge by providing at least €130 million
-            in funding from AFD-Proparco group, and a whole host of support
-            programs designed to meet the needs of Africa’s innovative
-            entrepreneurs.
+            Our vision is to foster a more inclusive and sustainable global
+            economy through ‘made in Africa’ digital innovation - and projects
+            that will become global benchmarks over time. The Africa we work
+            with on a daily basis is undoubtedly the new driver of global
+            growth; it is also a laboratory of ideas that can inspire all
+            regions of the world, both emerging and developed ones. By playing a
+            bridging role between Africa and the rest of the world with
+            enthusiasm and conviction, we help to draw attention to African
+            solutions and to connect Africa to global value chains. That way, we
+            define a new model of co-development around the common interests
+            that brings Africa and its international partners closer together.
           </p>
         </div>
 
         <div className="contact-cta-box">
-          <h3>Want to know more?</h3>
-          <p>Don't hesitate to contact us</p>
+          <h3>Do not miss updates</h3>
+          <p>Follow us on</p>
 
-          <Link href="/contact">
-            <a className="btn btn-primary">Contact Us</a>
+          <Link
+            href="https://www.linkedin.com/company/digitalafrica/"
+            target="_blank"
+          >
+            <a target="_blank" className="btn btn-primary">
+              Linkedin
+            </a>
           </Link>
         </div>
       </div>
