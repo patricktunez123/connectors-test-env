e0312a3ad212791ca9edc0fbf8b6e5c47ace59d4
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2022-11-25 13:02:04 +0200

    doneee

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/_App/Navbar";
 import MainBanner from "../components/Connectors/MainBanner";
 import AboutUs from "../components/Connectors/AboutUs";
@@ -7,18 +7,43 @@ import Connectors from "../components/Common/Connectors";
 import FunFacts from "../components/Connectors/FunFacts";
 import Footer from "../components/_App/Footer";
 import WelcomeNote from "../components/Connectors/WelcomeNote";
+import { useRouter } from "next/router";
 
 const HomePage = () => {
+  const router = useRouter();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setIsReady(true);
+    }
+  }, [router]);
+
   return (
     <>
-      <Navbar />
-      <MainBanner />
-      <Connectors />
-      <WelcomeNote />
-      <AboutUs />
-      {/* <BannerTwo /> */}
-      <FunFacts />
-      <Footer />
+      {!isReady ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <MainBanner />
+          <Connectors />
+          <WelcomeNote />
+          <AboutUs />
+          {/* <BannerTwo /> */}
+          <FunFacts />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
