53fe39a0a016f44720318bb48d6d95d38a45489e
Patrick TUNEZERWANE <tunezepatrick@gmail.com>
2022-11-25 11:37:57 +0200

    done

--- components/Common/Connectors.js
+++ components/Common/Connectors.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import * as Icon from "react-feather";
 import { ConnectorsArray } from "../Data/ConnectorsArray";
 import { limitStringLength } from "../helpers/limitStringLength";
+import { Button, Modal } from "antd";
 
 const Connectors = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [connector, setConnector] = useState();
+  const showModal = (item) => {
+    setConnector(item);
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  console.log("connector", connector);
+
   return (
     <div
       id="Connectors"
@@ -73,9 +89,9 @@ const Connectors = () => {
 
                   <p>
                     {item?.desc && limitStringLength(item?.desc)}{" "}
-                    <span>
-                      <u>read more</u>
-                    </span>
+                    <Button onClick={() => showModal(item)} type="text">
+                      read more
+                    </Button>
                   </p>
                 </div>
               </div>
@@ -83,6 +99,58 @@ const Connectors = () => {
           ))}
         </div>
       </div>
+
+      <Modal
+        title={connector?.name}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+        className="connector_modal"
+        footer={false}
+      >
+        <div className="row justify-content-center">
+          <div key={connector?.id} className="col-lg-12 col-md-12">
+            <div className="single-team">
+              <div className="team-image">
+                <img src="/images/team-image/team1.jpg" alt="image" />
+              </div>
+
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>{connector?.name}</h3>
+                  <span>{connector?.post}</span>
+                </div>
+
+                <ul>
+                  <li>
+                    <a href="https://www.facebook.com/" target="_blank">
+                      <Icon.Facebook />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.twitter.com/" target="_blank">
+                      <Icon.Twitter />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/" target="_blank">
+                      <Icon.Linkedin />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/" target="_blank">
+                      <Icon.Instagram />
+                    </a>
+                  </li>
+                </ul>
+
+                <p>{connector?.desc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
